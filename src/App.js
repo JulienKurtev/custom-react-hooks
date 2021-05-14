45f1ac1d89f9d44ce7b2525ee29b9b91aa3b81f8
JulienKurtev <julienemilovkurtev@gmail.com>
2021-05-13 23:03:24 -0400

    Add files via upload
    init commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import useHttp from './hooks/use-http';
+
+import Tasks from './components/Tasks/Tasks';
+import NewTask from './components/NewTask/NewTask';
+
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
+
+  useEffect(() => {
+    const transformTask = taskObj => {
+      const loadedTasks = [];
+
+      for (const taskKey in taskObj) {
+        loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+    }
+
+    fetchTasks({ url: 'https://custom-hooks-f3784-default-rtdb.firebaseio.com/tasks.json' }, transformTask);
+  }, [fetchTasks]);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </React.Fragment>
+  );
+}
+
+export default App;
